8781e67589a3d01658e53638620af267b5ea1fe4
Outofnothing <chriszhou0628@gmail.com>
2019-04-06 20:33:08 +0800

    init arc interpolation

--- dda.py
+++ dda.py
@@ -11,10 +11,16 @@ def dda_line(x0, y0, x1, y1):
     x_pulses = []
     y_pulses = []
     m = 2**n
-    delta_x = (x1-x0)/m
-    delta_y = (y1-y0)/m
+    if dx == 0:
+        delta_x = 0
+    else:
+        delta_x = (x1-x0)/dx
+    if dy == 0:
+        delta_y = 0
+    else:
+        delta_y = (y1-y0)/dy
     count = 0
-    x_now = 0
+    x_now = 0                                                                                                                                                       
     y_now = 0
     while count < m:
         x_now += dx
@@ -36,18 +42,101 @@ def dda_line(x0, y0, x1, y1):
     y_coordinates = [y0]
     for pulse in x_pulses:
         if pulse == 1:
-            x_coordinates.append(x_coordinates[-1]+delta_x/abs(delta_x))
+            x_coordinates.append(x_coordinates[-1]+delta_x)
 
         else:
             x_coordinates.append(x_coordinates[-1])
 
     for pulse in y_pulses:
         if pulse == 1:
-            y_coordinates.append(y_coordinates[-1] + delta_y/abs(delta_y))
+            y_coordinates.append(y_coordinates[-1] + delta_y)
         else:
             y_coordinates.append(y_coordinates[-1])
 
     # 输出计算过程
 
+    output_string = "计算过程：\n"
+    return x_coordinates, y_coordinates, output_string, m
+
+
+def dda_arc(x0, y0, x1, y1):
+    dy = abs(y1 - y0)
+    dx = abs(x1 - x0)
+    if dy >= dx:
+        temp = dy
+    else:
+        temp = dx
+    n = 1 
+    while 2**n < temp:
+        n += 1
+    m = 2**n
+    J_vx = y0
+    J_vy = x0
+    J_rx = 0.0
+    J_ry = 0.0
+    if dx == 0:
+        delta_x = 0
+    else:
+        delta_x = (x1-x0)/dx
+    if dy == 0:
+        delta_y = 0
+    else:
+        delta_y = (y1-y0)/dy
+    count_x = dx
+    count_y = dy
+    x_pulses = []
+    y_pulses = []
+    while (count_x > 0 or count_y > 0):
+        if count_x > 0 and count_y > 0:
+            J_rx += J_vx
+            if J_rx >= m:
+                J_rx -= m
+                x_pulses.append(1)
+                J_vy += delta_x
+                count_x -= 1
+            else:
+                x_pulses.append(0)
+            J_ry += J_vy
+            if J_ry >= m:
+                J_ry -= m
+                y_pulses.append(1)
+                J_vx += delta_y
+                count_y -= 1
+            else:
+                y_pulses.append(0)
+        if count_y > 0 and count_x == 0:
+            J_ry += J_vy
+            if J_ry >= m:
+                J_ry -= m
+                y_pulses.append(1)
+                J_vx += delta_y
+                count_y -= 1
+            else:
+                y_pulses.append(0)
+            x_pulses.append(0)
+        if count_x >0 and count_y == 0:
+            J_rx += J_vx
+            if J_rx >= m:
+                J_rx -= m
+                x_pulses.append(1)
+                J_vy += delta_x
+                count_x -= 1
+            else:
+                x_pulses.append(0)
+            y_pulses.append(0)
+    x_coordinates = [x0]
+    y_coordinates = [y0]
+    for pulse in x_pulses:
+        if pulse == 1:
+            x_coordinates.append(x_coordinates[-1]+delta_x)
+
+        else:
+            x_coordinates.append(x_coordinates[-1])
+
+    for pulse in y_pulses:
+        if pulse == 1:
+            y_coordinates.append(y_coordinates[-1] + delta_y)
+        else:
+            y_coordinates.append(y_coordinates[-1])
     output_string = "计算过程：\n"
     return x_coordinates, y_coordinates, output_string, m
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import dda
 import sys
+import dda
 import matplotlib.pyplot as plt
 from matplotlib import animation
 from PyQt5 import uic, QtWidgets
@@ -78,7 +78,65 @@ class MyApp(QtWidgets.QMainWindow, Ui_MainWindow):
         plt.show()
 
     def dda_arc(self):
-        pass
+        x0 = float(self.x0_input.toPlainText())
+        y0 = float(self.y0_input.toPlainText())
+        xe = float(self.xe_input.toPlainText())
+        ye = float(self.ye_input.toPlainText())
+        x_coordinates, y_coordinates, output_string, m = dda.dda_arc(x0, y0, xe, ye)
+
+        self.results_window.setText(output_string)
+
+        # First set up the figure, the axis, and the plot element we want to animate
+        fig = plt.figure()
+        plt.title("dda line interpolation")
+
+        # 设置合适坐标轴
+        if x0 > xe:
+            x_start = xe - abs(x0-xe)/10
+            x_end = x0 + abs(x0-xe)/10
+        if x0 < xe:
+            x_start = x0 - abs(x0-xe)/10
+            x_end = xe + abs(x0-xe)/10
+        if x0 == xe:
+            x_start = x0 - 5
+            x_end = x0 + 5
+        if y0 > ye:
+            y_start = ye - abs(y0-ye)/10
+            y_end = y0 + abs(y0-ye)/10
+        if y0 < ye:
+            y_start = y0 - abs(y0-ye)/10
+            y_end = ye + abs(y0-ye)/10
+        if y0 == ye:
+            y_start = y0 - 5
+            y_end = y0 + 5
+
+        ax = plt.axes(xlim=(x_start, x_end), ylim=(y_start, y_end))
+        line, = ax.plot([], [], lw=2)
+        plt.plot([x0, xe], [y0, ye], 'g')
+        plt.scatter([x0, xe], [y0, ye])
+        # initialization function: plot the background of each frame
+
+        def init():
+            line.set_data([], [])
+            return line,
+
+        # animation function.  This is called sequentially
+        def animate(i):
+            if i < len(x_coordinates):
+                x = [x_coordinates[0:i]]
+                y = [y_coordinates[0:i]]
+            else:
+                x = x_coordinates
+                y = y_coordinates
+            line.set_data(x, y)
+            return line,
+
+        # call the animator.  blit=True means only re-draw the parts that have changed.
+        anim = animation.FuncAnimation(fig, animate, init_func=init,
+                                       frames=m*20, interval=30, blit=True)
+        print(x_coordinates)
+        print(y_coordinates)
+        plt.show()
 
 
 if __name__ == "__main__":
